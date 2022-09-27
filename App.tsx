634b4b729dae5a208f51039bd8b93ca33d9694bf
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-09-27 12:22:13 +0200

    Basic navigation

--- App.tsx
+++ App.tsx
@@ -1,25 +1,47 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet } from 'react-native';
+import Home from './components/screens/home/Home';
+import Search from './components/screens/search/Search';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Books from './components/screens/books/Books';
+import Lists from './components/screens/lists/Lists';
 
 const App: () => JSX.Element = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <View>
-          <Text>Lorem ipsum</Text>
-        </View>
-      </SafeAreaView>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = 'alert';
+
+            if (route.name === 'Home')
+              iconName = focused ? 'home' : 'home-outline';
+
+            if (route.name === 'Search')
+              iconName = focused ? 'search' : 'search-outline';
+
+            if (route.name === 'Books')
+              iconName = focused ? 'book' : 'book-outline';
+
+            if (route.name === 'Lists')
+              iconName = focused ? 'checkbox' : 'checkbox-outline';
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#355070',
+          tabBarInactiveTintColor: '#6D6D6D',
+          headerShown: false,
+        })}
+        initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Books" component={Books} />
+        <Tab.Screen name="Lists" component={Lists} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
