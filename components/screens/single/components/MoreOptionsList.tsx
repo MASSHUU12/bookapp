342b429a634e754b1adebb569bd5cb5600bffad6
Jacqouese <jakub.czaja003@gmail.com>
2022-10-07 15:42:24 +0200

    remove book from history option

--- components/screens/single/components/MoreOptionsList.tsx
+++ components/screens/single/components/MoreOptionsList.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import React, { useEffect } from 'react';
 import OptionsBtn from '../../../common/OptionsBtn';
 import { BookType } from '../../../../types/bookType';
 import { useGlobalState } from '../../../../hooks';
+import sql from '../../../../services/sql/sql';
 
 type PropsTypes = {
   bookData: BookType | {};
@@ -11,16 +12,40 @@ type PropsTypes = {
 const MoreOptionsList = ({ bookData }: PropsTypes) => {
   const [state, dispatch] = useGlobalState();
 
+  const handleRemoveBookFromHistory = () => {
+    if (!('key' in bookData)) return;
+
+    return Alert.alert(
+      'Are your sure?',
+      'Removing a book from history is irreversible',
+      [
+        // The "Yes" button
+        {
+          text: 'Yes',
+          onPress: () => {
+            sql.removeBookFromHistory(bookData.key);
+            dispatch(1);
+          },
+        },
+        {
+          text: 'No',
+        },
+      ],
+    );
+  };
+
   const allOptionsAction = [
     () => null,
     () => null,
     () => console.log('Mark as favorite'),
+    () => handleRemoveBookFromHistory(),
   ];
 
   const allOptionsText = [
     'Add to read later',
     'Add to currently reading',
     'Add to already read',
+    'Remove book from history',
   ];
 
   const allOptionsMap = {
