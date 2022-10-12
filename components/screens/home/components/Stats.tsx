3d429987266ad2287247d41c888531e3a19d987b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2022-10-12 13:47:17 +0200

    Dependencies cleanup

--- components/screens/home/components/Stats.tsx
+++ components/screens/home/components/Stats.tsx
@@ -1,6 +1,4 @@
 import { View } from 'react-native';
-import { ProgressChart } from 'react-native-chart-kit';
-import { Dimensions } from 'react-native';
 import P from '../../../common/P';
 import { t } from '../../../../i18n/strings';
 import { useTheme } from '@react-navigation/native';
@@ -13,30 +11,10 @@ import { useTheme } from '@react-navigation/native';
 const Stats = (): JSX.Element => {
   const { colors } = useTheme();
 
-  const chartConfig = {
-    backgroundGradientFrom: colors.background,
-    backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: colors.background,
-    backgroundGradientToOpacity: 0,
-    color: (opacity = 1) => `rgba(159, 159, 159, ${opacity})`,
-    useShadowColorFromDataset: false,
-  };
-
   return (
     <View>
       <P>{t.stats1}</P>
-      <ProgressChart
-        data={{
-          labels: [t.miscYear, t.miscMonth],
-          data: [0.6, 0.4],
-        }}
-        width={Dimensions.get('window').width * 0.75}
-        height={Dimensions.get('window').height * 0.2}
-        strokeWidth={12}
-        radius={16}
-        chartConfig={chartConfig}
-        hideLegend={false}
-      />
+      <P>Chart.</P>
     </View>
   );
 };
